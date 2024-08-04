1db227bd56e62b8eb0ffee117f369fa3cbc5db7f
MrNumiko <tim.raida@gmail.com>
2024-08-04 11:43:58 +0200

    feat: First steps

--- app/src/main/java/com/example/doppel_moppel/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/doppel_moppel/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.doppel_moppel.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.doppel_moppel.databinding.FragmentHomeBinding;
 
@@ -18,15 +20,33 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnConfirm.setBackgroundColor(Color.GREEN);
+        binding.btnConfirm.setOnClickListener(v -> enterParticipant());
+
+        binding.btnUpdateList.setBackgroundColor(Color.YELLOW);
+
+        binding.btnClear.setBackgroundColor(Color.RED);
+        binding.btnClear.setOnClickListener(v -> clearInputs());
+    }
+
+    private void enterParticipant() {
+        String inputParticipant = binding.inputParticipants.getEditableText().toString();
+        if (!inputParticipant.isEmpty()) {
+            binding.txtEnteredParticipants.append("\n" + inputParticipant);
+        }
+    }
+
+    private void clearInputs() {
+        binding.txtEnteredParticipants.setText("");
     }
 
     @Override
